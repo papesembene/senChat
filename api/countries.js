3c34e77921296e43c57c7ea2d0d606e599a4b222
pape sembene <sembenpape4@gmail.com>
2025-06-06 13:51:26 +0000

    manque un params dans url

--- api/countries.js
+++ api/countries.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
      console.log("API countries appelée !");
-  const response = await fetch('https://restcountries.com/v3.1/all', { headers: { 'Accept': 'application/json' } });
+     const response = await fetch('https://restcountries.com/v3.1/all?fields=name,idd,cca2', { headers: { 'Accept': 'application/json' } });
   const data = await response.json();
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json(data);
